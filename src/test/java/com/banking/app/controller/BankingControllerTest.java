e082fab1c03010d3a7d98055dbf22c84970297f3
Pranas Kieras <pranas.kieras@swedbank.lt>
2020-03-16 12:39:30 +0200

    added base controller and failing test

---
+++ src/test/java/com/banking/app/controller/BankingControllerTest.java
@@ -0,0 +1,17 @@
+package com.banking.app.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+public class BankingControllerTest {
+
+    @Test
+    public void signUpUser_ShouldCall_UserSignUpService(){
+        fail();
+    }
+
+}
