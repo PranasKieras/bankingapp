e8fa72f125bd1ff9d94f4b6718434b3217c095a5
Pranas Kieras <pranas.kieras@swedbank.lt>
2020-03-19 17:05:46 +0200

    renamed main class to proper name

--- src/test/java/com/banking/app/BankingApplicationTests.java
+++ src/test/java/com/banking/app/BankingApplicationTests.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class AppApplicationTests {
+class BankingApplicationTests {
 
     @Autowired
     private MockMvc mvc;
